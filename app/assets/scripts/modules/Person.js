91ceee9bd287aaeec1cae7001d292fc64812a357
Stanislav Dashkov <stdashkov@mail.ru>
2017-10-25 19:50:50 +0300

    add click function

--- app/assets/scripts/modules/Person.js
+++ app/assets/scripts/modules/Person.js
@@ -2,14 +2,22 @@
 var $ = require('jquery');
 
 //Specific By CLICKING
-$('li').click(function () {
+$('ul').on("click", "li", function () {
   $(this).toggleClass('completed');
-
 });
 
-$('span').click(function(event) {
-  $(this).parent().fadeOut(500, function(){
+$('ul').on('click',"span", function(event) {
+  $(this).parent().fadeOut(500,function(){
     $(this).remove();
   });
   event.stopPropagation();
+});
+
+$('input').keypress(function(event) {
+  if(event.which === 13) {
+    var todoText = $(this).val();
+    $(this).val("");
+
+    $("ul").append('<li><span>X </span>' + todoText + '</li>')
+  }
 });
--- app/temp/scripts/App.js
+++ app/temp/scripts/App.js
@@ -77,15 +77,24 @@
 var $ = __webpack_require__(3);
 
 //Specific By CLICKING
-$('li').click(function () {
+$('ul').on("click", "li", function () {
   $(this).toggleClass('completed');
 });
 
-$('span').click(function (event) {
+$('ul').on('click', "span", function (event) {
   $(this).parent().fadeOut(500, function () {
     $(this).remove();
   });
   event.stopPropagation();
+});
+
+$('input').keypress(function (event) {
+  if (event.which === 13) {
+    var todoText = $(this).val();
+    $(this).val("");
+
+    $("ul").append('<li><span>X </span>' + todoText + '</li>');
+  }
 });
 
 /***/ }),
